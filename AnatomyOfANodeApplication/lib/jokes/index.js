b55adf3b5165db2e5961064e036590e5f39181a3
Ian Salt <ian@isalt.digital>
2018-06-11 16:23:39 +0100

    Anatomy of a Node Application example

---
+++ AnatomyOfANodeApplication/lib/jokes/index.js
@@ -0,0 +1,27 @@
+/*
+  *  Title:  Jokes index file
+  *  Description: Joke library
+  *  Author:  Ian Salt
+  *  Date: 11/06/2018
+  *
+  */
+
+// Dependencies
+const fs = require('fs');
+
+//  App object
+const jokes = {};
+
+//  Get all teh jokes and return them to the user
+jokes.allJokes = function () {
+  // Read the text file containing the jokes
+      const fileContents = fs.readFileSync(`${__dirname}/jokes.txt`, 'utf8');
+
+      // Turn the string into an array
+      const arrayOfJokes = fileContents.split(/\r?\n/);
+
+      // Return the array
+      return arrayOfJokes;
+};
+//  Export the Library
+module.exports = jokes;
